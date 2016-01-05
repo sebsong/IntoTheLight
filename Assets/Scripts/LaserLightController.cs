d036b11603cc7d5b81159870a01e53ca044fd8d3
Sebastian Song <sebastianbsong@gmail.com>
2016-01-04 17:20:19 -0800

    added progress gif for later ccomparison

--- Assets/Scripts/LaserLightController.cs
+++ Assets/Scripts/LaserLightController.cs
@@ -4,17 +4,19 @@ using System.Collections;
 public class LaserLightController : MonoBehaviour {
 	Light pointLight;
 	float laserIntensity;
+	float fadeSpeed;
 
 	// Use this for initialization
 	void Awake () {
 		pointLight = gameObject.GetComponent<Light> ();
 		laserIntensity = pointLight.intensity;
+		fadeSpeed = laserIntensity * 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (pointLight.intensity > 0) {
-			pointLight.intensity -= 9 * Time.deltaTime;
+			pointLight.intensity -= fadeSpeed * Time.deltaTime;
 		}
 	}
 
